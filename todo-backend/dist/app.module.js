74604556282bd81d68b068aa6ff4a72ec23651a3
LX5321 <alexanderwebsite5321@gmail.com>
2023-06-12 22:21:49 +0530

    Added TODO module

--- todo-backend/dist/app.module.js
+++ todo-backend/dist/app.module.js
@@ -10,11 +10,12 @@ exports.AppModule = void 0;
 const common_1 = require("@nestjs/common");
 const app_controller_1 = require("./app.controller");
 const app_service_1 = require("./app.service");
+const todo_module_1 = require("./todo/todo.module");
 let AppModule = exports.AppModule = class AppModule {
 };
 exports.AppModule = AppModule = __decorate([
     (0, common_1.Module)({
-        imports: [],
+        imports: [todo_module_1.TodoModule],
         controllers: [app_controller_1.AppController],
         providers: [app_service_1.AppService],
     })
