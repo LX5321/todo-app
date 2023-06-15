5df146ca0ae46b9ae114c353ef619e070017707a
LX5321 <alexanderwebsite5321@gmail.com>
2023-06-15 10:51:51 +0530

    Imported Validator Pipe

--- todo-backend/src/todo/tasks.controller.ts
+++ todo-backend/src/todo/tasks.controller.ts
@@ -1,6 +1,8 @@
-import { Controller, Get, NotFoundException, Param } from '@nestjs/common';
+import { Body, Controller, Get, NotFoundException, Param, Post, UsePipes } from '@nestjs/common';
 import { TaskService } from './task.service';
 import { TaskModel } from './task.model';
+import { ValidationPipe } from '../../pipes/ValidationPipe';
+import { ApiBody } from '@nestjs/swagger';
 
 @Controller('tasks')
 export class TasksController {
@@ -21,5 +23,13 @@ export class TasksController {
         }
 
         return task;
+    }
+
+    @Post()
+    @UsePipes(new ValidationPipe())
+    @ApiBody({ type: TaskModel, examples: { example1: { value: { id: 1, completed: false, description: "A sample" } } } })
+    async addTask(@Body() data: TaskModel) {
+        this.taskService.addTaskToList(data);
+        return data;
     }
 }
